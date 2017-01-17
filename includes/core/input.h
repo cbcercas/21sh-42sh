45bd2d592097c2e22553deb04ca0c28ef6764901
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-01-17 17:07:16 +0100

    input: add input support

---
+++ includes/core/input.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/16 13:28:56 by chbravo-          #+#    #+#             */
+/*   Updated: 2017/01/17 15:35:06 by chbravo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef INPUT_H
+# define INPUT_H
+# include "libft.h"
+# include "get_next_line.h"
+# include "init.h"
+# include "parsing_command.h"
+# include "command.h"
+
+int	ms_get_command(t_ms_data	*data);
+
+#endif
---
+++ srcs/core/input.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/16 13:28:12 by chbravo-          #+#    #+#             */
+/*   Updated: 2017/01/17 16:30:41 by chbravo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "input.h"
+
+int	ms_get_command(t_ms_data *data)
+{
+	t_command	*com_list;
+	t_command	*e;
+	char		*command;
+	int			ret;
+
+	com_list = NULL;
+	ret = get_next_line(0, &command);
+	if (ret == 1 && ft_strcmp(command, ""))
+		com_list = ms_parse_command(command);
+	e = com_list;
+	while (e)
+	{
+		if (ms_command(data, e))
+			return (1);
+		e = e->next;
+	}
+	ft_strdel(&command);
+	return (0);
+}
