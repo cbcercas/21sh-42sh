8995a5bb56107514329831bca058faa4fc382dfe
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-01-20 13:49:28 +0100

    forgot input header

--- includes/main.h
+++ includes/main.h
@@ -6,7 +6,7 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/15 20:31:43 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/01/16 11:46:32 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/01/16 13:36:33 by chbravo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 # define MAIN_H
 # include "init.h"
 # include "prompt.h"
-
+# include "input.h"
 #endif
