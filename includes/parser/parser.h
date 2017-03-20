fd0275ab9352094f0325dc5a3ecef617c0cf415c
Jules LASNE <jlasne@student.42.fr>
2017-03-20 10:51:52 +0100

    Parser: Added the Doxygen for the parser

--- includes/parser/parser.h
+++ includes/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 10:02:17 by jlasne            #+#    #+#             */
-/*   Updated: 2017/03/20 10:03:57 by jlasne           ###   ########.fr       */
+/*   Updated: 2017/03/20 10:32:58 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define PARSER_H
 
 # include <lexer/lexer.h>
+
+/*
+** @file parser.c
+**
+** @brief initialize the parser
+*/
 
 void	parser_init(t_array *tokens, char *input);
 
--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,15 +6,19 @@
 /*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 09:43:16 by jlasne            #+#    #+#             */
-/*   Updated: 2017/03/20 10:07:39 by jlasne           ###   ########.fr       */
+/*   Updated: 2017/03/20 10:41:34 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** HERE WILL BE A PARSER
-*/
-
 #include <parser/parser.h>
+
+/*
+** @brief Initializes the parser for the program
+**
+** @param tokens  The tokens sent by the lexer
+** @param input   The input the user sent
+** @return Returns nothing for now
+**/
 
 void	parser_init(t_array *tokens, char *input)
 {
