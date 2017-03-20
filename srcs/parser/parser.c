540f7f25921f814d7bb554825f9a09f00f44fbf0
Jules LASNE <jlasne@student.42.fr>
2017-03-20 12:53:43 +0100

    Parser: Added count of tokens and log for it

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -24,9 +24,33 @@
 
 void	parser_init(t_array *tokens, char *input)
 {
+	size_t		i;
+	size_t nb_word;
+	size_t nb_blank;
+	size_t nb_newline;
+	t_token *tok;
+
+	nb_blank = 0;
+	nb_word = 0;
+	nb_newline = 0;
+	i = 0;
 	logger_init(7, PROG_NAME, "log.log");
 	log_info("Parser: initializing");
 	log_info("Parser: user input is %s", input);
 	log_info("Parser: input is %zu long.", ft_strlen(input));
 	log_info("Parser: lexer returned %zu tokens to be parsed", tokens->used);
+	while (i < tokens->used)
+	{
+		tok = (t_token *)array_get_at(tokens, i);
+		if (tok->type == E_TOKEN_WORD)
+			nb_word++;
+		else if (tok->type == E_TOKEN_BLANK)
+			nb_blank++;
+		else if (tok->type == E_TOKEN_NEWLINE)
+			nb_newline++;
+		i++;
+	}
+	log_info("Found %zu words", nb_word);
+	log_info("Found %zu blank spaces", nb_blank);
+	log_info("Found %zu newlines", nb_newline);
 }
