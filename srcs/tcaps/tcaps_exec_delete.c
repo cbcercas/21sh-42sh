2e4fddd034452d22c9b0365dca13d8e910830cba
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-05-19 21:47:35 +0200

    Fix delete, add beep when no deletion possible

--- srcs/tcaps/tcaps_exec_delete.c
+++ srcs/tcaps/tcaps_exec_delete.c
@@ -16,10 +16,15 @@ BOOL	exec_delete(const t_key *key, t_input *input)
 {
 	(void)key;
 	log_dbg1("exec delete.");
-	string_remove_char(input->str, input->cpos.cp_col - input->offset);
-	tputs(tgetstr("dm", NULL), 0, &ft_putchar2);
-	tputs(tgetstr("dc", NULL), 0, &ft_putchar2);
-	tputs(tgetstr("de", NULL), 0, &ft_putchar2);
+	if (input->str->len >= input->cpos.cp_col - input->offset)
+	{
+		string_remove_char(input->str, input->cpos.cp_col - input->offset - 1);
+		tputs(tgetstr("dm", NULL), 0, &ft_putchar2);
+		tputs(tgetstr("dc", NULL), 0, &ft_putchar2);
+		tputs(tgetstr("de", NULL), 0, &ft_putchar2);
+	}
+	else
+		write(1, "\a", 1);
 	return (false);
 }
 
