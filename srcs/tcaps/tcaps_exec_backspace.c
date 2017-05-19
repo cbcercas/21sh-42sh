0ea474628720183a070a3c9c51e9f4b4a4e7af7d
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-05-19 22:10:55 +0200

    Add backspace support

--- srcs/tcaps/tcaps_exec_backspace.c
+++ srcs/tcaps/tcaps_exec_backspace.c
@@ -15,7 +15,17 @@
 BOOL	exec_backspace(const t_key *key, t_input *input)
 {
 	(void)key;
-	write(1, "User pressed backspace\n", 22);
-	exec_ctrl_c(key, input);
+	log_dbg3("User pressed backspace");
+	if (input->cpos.cp_col > input->offset + 1)
+	{
+		string_remove_char(input->str, input->cpos.cp_col - input->offset - 2);
+		tputs(tgetstr("le", NULL), 0, &ft_putchar2);
+		input->cpos = get_cursor_pos();
+		tputs(tgetstr("dm", NULL), 0, &ft_putchar2);
+		tputs(tgetstr("dc", NULL), 0, &ft_putchar2);
+		tputs(tgetstr("de", NULL), 0, &ft_putchar2);
+	}
+	else
+		write(1, "\a", 1);
 	return (false);
 }
