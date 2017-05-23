e9e970c87cdfee9e36315bc29798c000c8fc12ab
Jules LASNE <jlasne@student.42.fr>
2017-05-23 14:00:56 +0200

    Parser: Added __FILENAME__

--- includes/parser/parser_grammar.h
+++ includes/parser/parser_grammar.h
@@ -8,6 +8,8 @@
 # include <lexer/lexer.h>
 # include <logger/logger.h>
 
+#define __FILENAME__ (ft_strrchr(__FILE__, '/') ? ft_strrchr(__FILE__, '/') + 1 : __FILE__)
+
 t_bool check_double(t_array *tokens, size_t where);
 
 t_bool check_last(t_array *tokens, size_t where);
