2fedbaaf2e50695cdebe02ce8c318054277e9f67
Jules LASNE <jlasne@student.42.fr>
2017-05-23 13:51:29 +0200

    Parser: Renamed parser_new into parser and added logs into parser functions

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -26,12 +26,16 @@ static void	dbg_printtok_str(t_array *tokens, size_t i)
 t_bool gr_checkcmd(t_array *tokens, size_t where)
 {
 
+	log_dbg2("Parser: Now checking if token %d is the first one and if it is correct", where);
 	if (check_first(tokens, where) == false)
 		return (false);
+	log_dbg2("Parser: Now checking if token %d is the last one and if it is correct", where);
 	if (check_last(tokens, where) == false)
 		return (false);
+	log_dbg2("Parser: Now checking if token %d is an operator and if it is surrounded correctly", where);
 	if (check_double(tokens, where) == false)
 		return (false);
+	log_dbg2("Parser: Now checking if token %d is a semicolon and if it is surrounded correctly", where);
 	if (check_double_semi(tokens, where) == false)
 		return (false);
 }
@@ -49,8 +53,11 @@ t_bool parser_parse(t_array *tokens)
 
 	where = 0;
 	ok = false;
+	log_info("Parser: in %s:%d is initialized.", __FILE__, __LINE__);
+	log_dbg1("Parser: Cycling through tokens.");
 	while (where <= tokens->used)
 	{
+		log_dbg2("Parser: Cycling through tokens: Now at token %d.", where);
 		dbg_printtok_str(tokens, where);
 		ok = gr_checkcmd(tokens, where);
 		if (ok == false)
@@ -59,9 +66,13 @@ t_bool parser_parse(t_array *tokens)
 	}
 	if (ok == false)
 	{
+		log_fatal("Parser: returned false at token %d.\n\nPARSER ERROR\n", where);
 		ft_printf("Parser Error\n");
 		return (false);
 	}
 	else
+	{
+		log_info("Parser: return true after cycling through all tokens.");
 		return (true);
+	}
 }
