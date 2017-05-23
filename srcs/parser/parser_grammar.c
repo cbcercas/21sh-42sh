1584c751da82eb14c9aa6d3ae652cc2482cc8869
Jules LASNE <jlasne@student.42.fr>
2017-05-23 10:57:48 +0200

    Added the check_first function
    This function will check if the first token isnt an operator

--- srcs/parser/parser_grammar.c
+++ srcs/parser/parser_grammar.c
@@ -2,4 +2,15 @@
 // Created by Jules LASNE on 5/23/17.
 //
 
-#include <parser/parser_grammar.h>
+#include <parser/parser_grammar.h>#include <parser/parser_grammar.h>
+
+t_bool check_first(t_array *tokens, size_t where)
+{
+	if (where == 0)
+		if (check_operators(tokens, where) == true)
+			return (false);
+		else
+			return (true);
+	else
+		return (true);
+}
