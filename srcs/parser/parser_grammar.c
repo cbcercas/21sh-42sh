d5f0a807bd9362956edc7c550c54bdca44853023
Jules LASNE <jlasne@student.42.fr>
2017-05-23 10:59:55 +0200

    Parser: Added the check_last function
    This function checks if the last token is an operator

--- srcs/parser/parser_grammar.c
+++ srcs/parser/parser_grammar.c
@@ -14,3 +14,14 @@ t_bool check_first(t_array *tokens, size_t where)
 	else
 		return (true);
 }
+
+t_bool check_last(t_array *tokens, size_t where)
+{
+	if (check_operators(tokens, where) == true)
+		if (where < tokens->used)
+			return (true);
+		else
+			return (false);
+	else
+		return (true);
+}
