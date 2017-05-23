485d23db6dc812f1fa510bf58fa915986878f627
Jules LASNE <jlasne@student.42.fr>
2017-05-23 11:05:29 +0200

    Added the check_operators function
    Like the check_word_blank function, but for operator :)

--- srcs/parser/parser_grammar.c
+++ srcs/parser/parser_grammar.c
@@ -36,3 +36,18 @@ t_bool check_word_blank(t_array *tokens, size_t where)
 	else
 		return (false);
 }
+
+t_bool check_operators(t_array *tokens, size_t where)
+{
+	t_token *tok;
+
+	tok = (t_token *)array_get_at(tokens, where);
+	if (tok->type == E_TOKEN_PIPE || tok->type == E_TOKEN_AND || \
+	tok->type == E_TOKEN_AND_IF || tok->type == E_TOKEN_DGREAT || \
+	tok->type == E_TOKEN_DLESS || tok->type == E_TOKEN_GREATAND || \
+ 	tok->type == E_TOKEN_LESSAND || tok->type == E_TOKEN_LESSGREAT || \
+	tok->type == E_TOKEN_OR_IF)
+		return (true);
+	else
+		return (false);
+}
