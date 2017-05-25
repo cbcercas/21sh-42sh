12485cf37ef1a0d5e2534416de4a7a06df164667
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-05-25 20:53:10 +0200

    Ui: Fix backspace and delete on 1 char

--- srcs/tcaps/tcaps_exec_delete.c
+++ srcs/tcaps/tcaps_exec_delete.c
@@ -18,11 +18,11 @@ BOOL	exec_delete(const t_key *key, t_input *input)
 
 	(void)key;
 	log_dbg1("exec delete.");
-	pos = (input->cpos.cp_col - (input->offset_col + 1)) +
+	pos = (input->cpos.cp_col + 1 - (input->offset_col + 1)) +
 		  (input->ts.ts_cols * input->offset_line);
 	if (input->str->len >= pos)
 	{
-		string_remove_char(input->str, pos + 1);
+		string_remove_char(input->str, pos);
 		tputs(tgetstr("dm", NULL), 0, &ft_putchar2);
 		tputs(tgetstr("dc", NULL), 0, &ft_putchar2);
 		tputs(tgetstr("de", NULL), 0, &ft_putchar2);
