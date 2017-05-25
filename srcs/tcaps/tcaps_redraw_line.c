0bb7a3073837735278987749b6861d4737e8b6a1
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-05-25 20:50:22 +0200

    Ui: Fix clean reste of screen in redraw line

--- srcs/tcaps/tcaps_redraw_line.c
+++ srcs/tcaps/tcaps_redraw_line.c
@@ -12,17 +12,29 @@ void	redraw_line(t_input *input)
 		  (input->ts.ts_cols * input->offset_line);
 	len = ft_strlen(tmp);
 	line = 0;
-	col = input->cpos.cp_col + len;
+	col = 0;
+	// clear rest of line if needed
+	tputs(tgetstr("ce", NULL), 0, ft_putchar2);
+	if ( len + 1 > (size_t)(input->ts.ts_cols - input->cpos.cp_col))
+	{
+		tputs(tgetstr("do", NULL), 0, ft_putchar2);
+		tputs(tgetstr("cr", NULL), 0, ft_putchar2);
+		tputs(tgetstr("cd", NULL), 0, ft_putchar2);
+		tputs(tgetstr("up", NULL), 0, ft_putchar2);
+		while (col < input->cpos.cp_col)
+		{
+			tputs(tgetstr("nd", NULL), 0, &ft_putchar2);
+			col += 1;
+		}
+	}
 	//affiche la ligne
 	tputs(tmp, (int) len, &ft_putchar2);
+	col = input->cpos.cp_col + len;
 	if (len > (size_t)(input->ts.ts_cols - input->cpos.cp_col))
 	{
 		line =(len - (input->ts.ts_cols - input->cpos.cp_col)) / input->ts.ts_cols + ((len - (input->ts.ts_cols - input->cpos.cp_col)) % input->ts.ts_cols? 1 : 0);
 		col  = (len - (input->ts.ts_cols - input->cpos.cp_col)) % input->ts.ts_cols;
 	}
-	// clear rest of line if needed
-	if (col)
-		tputs(tgetstr("ce", NULL), 0, ft_putchar2);
 	//TODO add tcaps_up(t_input *input, int nb)
 	while (line)
 	{
