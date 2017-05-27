5267f658499b3b6122333216f478ebd8ebf40b28
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-05-27 19:45:39 +0200

    [FIX]Parser: fix PROGNAME macro

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -9,6 +9,6 @@ t_array	*parser_parse(t_array *tokens)
 	tok = array_get_at(tokens, 0);
 	if (tok->type == E_TOKEN_WORD)
 		return tokens;
-	ft_printf("%s: parsing error or not supported\n", PROG_NAME);
+	ft_printf("%s: parsing error or not supported\n", PROGNAME);
 	return (NULL);
 }
