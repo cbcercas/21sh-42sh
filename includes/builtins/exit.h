ec625059a29201ea938d344caf5cc76c4efdfccc
Guillaume Pouyat <guiforge@gmail.com>
2017-06-12 11:34:41 +0200

    [REFACTOR]Builtin: update exit

--- includes/builtins/exit.h
+++ includes/builtins/exit.h
@@ -6,14 +6,18 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 13:49:31 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/05/17 16:47:28 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/06/12 11:21:31 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef EXIT_H
 # define EXIT_H
 # include <ftprintf.h>
 # include <core/data.h>
+# include <builtins/builtin_history.h>
+# include <core/tcaps.h>
+# include <core/init.h>
 
-int	sh_exit(t_sh_data *data, char *arg);
+int	sh_exit(t_sh_data *data, char **arg);
 
 #endif
--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,15 +6,21 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 13:49:07 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/02/15 11:01:47 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/06/12 11:22:00 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <builtins/exit.h>
 
-int	sh_exit(t_sh_data *data, char *arg)
+int	sh_exit(t_sh_data *data, char **arg)
 {
-	(void)arg;
-	(void)data;
+	int		status;
+
+	status = 0;
+	sh_history_save();
+	sh_deinit(data);
+	ft_putstr("\033[?1049l");
+	default_terminal_mode();
 	ft_printf("exit\n");
-	return (1);
+	exit(status);
 }
