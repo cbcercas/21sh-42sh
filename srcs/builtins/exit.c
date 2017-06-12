b0cc4c59c9ea5624a655a8dc41c77b8519cf753f
guiforge <guiforge@gmail.com>
2017-06-12 20:12:55 +0200

    [REFACTOR]Builtin: exit done

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 13:49:07 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/06/12 11:22:00 by gpouyat          ###   ########.fr       */
+/*   Updated: 2017/06/12 20:06:04 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,7 @@ int	sh_exit(t_sh_data *data, char **arg)
 	ft_putstr("\033[?1049l");
 	default_terminal_mode();
 	ft_printf("exit\n");
+	if (arg && arg[1] && ft_isdigit(arg[1][0]))
+		status = ft_atoi(arg[1]);
 	exit(status);
 }
