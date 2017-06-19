258b8724f7ed3896ee086e1e661475c4e4a27c8c
Jules Lasne (jlasne) <jlasne@student.42.fr>
2017-06-19 23:20:25 +0200

    [REFACTOR] Builtins: Updated from local merge

--- srcs/builtins/builtin_help.c
+++ srcs/builtins/builtin_help.c
@@ -12,10 +12,20 @@
 
 #include <builtins/help.h>
 
-int		sh_builtin_help(t_sh_data *data, char **args)
+void sh_help_default(t_sh_data *data)
 {
-	(void)args;
-	(void)data;
-	ft_printf("Hi ! I'm help, how can i help ?\n");
+	ft_printf("#-- %s V0.42 --#\n", PROGNAME);
+	ft_printf("These shell commands are defined internally.  Type `help' to");
+	ft_printf("see this list.\n");
+	ft_printf("Type `help name' to find out more about the function `name'.\n");
+	ft_printf("\n\nA star (*) next to a name means that the command is disabled.\n");
+}
+
+int sh_builtin_help(t_sh_data *data, char **args)
+{
+	(void) args;
+	(void) data;
+	if (args[1] == NULL)
+		sh_help_default(data);
 	return (0);
 }
