be66b7d76f1db0a9be47d1dd7238df698c55abab
Jules LASNE <jlasne@student.42.fr>
2017-06-19 14:32:19 +0200

    [NORM] Builtins: Normed builtin_help.c file

--- srcs/builtins/builtin_help.c
+++ srcs/builtins/builtin_help.c
@@ -1,10 +1,18 @@
-//
-// Created by seluj78 on 16/06/17.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_help.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/19 14:29:44 by jlasne            #+#    #+#             */
+/*   Updated: 2017/06/19 14:30:43 by jlasne           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <builtins/help.h>
 
-int sh_builtin_help(t_sh_data *data, char **args)
+int		sh_builtin_help(t_sh_data *data, char **args)
 {
 	(void)args;
 	(void)data;
