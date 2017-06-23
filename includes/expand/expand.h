7c712b489612f6f28ff40c553d5270546a8b3207
mleroyy <mleroy@student.42.fr>
2017-06-23 18:12:49 +0200

    [FEATURE]Expand: include updated

---
+++ includes/expand/expand.h
@@ -0,0 +1,7 @@
+#include <lexer/lexer.h>
+#include <core/main.h>
+
+t_array *expand_exp(t_array *tokens, t_array *tokens_expended);
+t_array	*expand_init(t_array *tokens_expended);
+
+void	test(t_array *toks);
