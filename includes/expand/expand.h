e7e86ca7fbf2ec8326981c19d3eaa649cf285b98
mleroyy <mleroy@student.42.fr>
2017-07-08 01:08:54 +0200

    [REFACTOR]Expand: separated files

--- includes/expand/expand.h
+++ includes/expand/expand.h
@@ -18,25 +18,19 @@
 
 typedef struct s_exp
 {
-	char 			    *str;
+	char 			*str;
 	t_token_type	type;
 }               t_exp;
 
-t_array	*expand(t_array *tokens, t_array *array_exp);
 t_array	*expand_init(t_array *expand_array);
+t_array	*expand(t_array *tokens, t_array *array_exp);
 t_exp	*exp_create_new(t_token *tok);
-void	sh_expand_destroy(t_array *array_exp);
-void	expand_print(t_array *array_exp);
-void	sh_exp_del(void *i);
-void	ft_expand_print_token(t_token_type type);
-t_array *ft_expand_duplication(t_exp *exp, t_array *tokens, t_array *array_exp);
-t_array	*ft_expand_history(t_array *array_exp, size_t index);
-
+t_exp	*expand_hist(t_exp *exp);
 char	*ft_replace_exp(char *src1, char *src2, int index, int size);
 
-t_array *expand_exp(t_array *tokens, t_array **tokens_expended);
-//t_array	*expand_init(t_array **tokens_expended);
+void	sh_exp_del(void *i);
+void	sh_expand_destroy(t_array *array_exp);
 
-void	test(t_array *toks);
-
+void	expand_print(t_array *array_exp);
+void	ft_expand_print_token(t_token_type type);
 #endif
