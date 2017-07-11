ade1336a229220458d27c214f348bd624a33a504
mleroyy <mleroy@student.42.fr>
2017-07-11 12:14:06 +0200

    [FEATURE]Expand: don't expand SQUOTE

--- srcs/expand/expand.c
+++ srcs/expand/expand.c
@@ -11,11 +11,10 @@ t_array *expand(t_array *tokens, t_array *array_exp)
   while (tokens && i < tokens->used)// dans tous les tokens
   {
     exp = exp_create_new((t_token *)array_get_at(tokens, i));// etape 1
-    expand_dol(exp);//expand les dollard
-    if (expand_hist(exp) == NULL)//expand les !
+    if (exp->type != E_TOKEN_SQUOTE)
     {
-      log_fatal("Expand: expand_hist failed");
-      return (NULL);
+        expand_dol(exp);//expand les dollard
+        expand_hist(exp);//expand les !
     }
     // expand_quote(exp);//expand quote
     array_push(array_exp, (void *)exp);// push dans notre t_array
