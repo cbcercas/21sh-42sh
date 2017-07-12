7397c69c368909f398d483f3ebaaafa4fa09e1c1
mleroyy <mleroy@student.42.fr>
2017-07-13 00:21:56 +0200

    [FIX]Expand: removal \ when no SQUOTE and only one \

--- srcs/expand/expand_quote.c
+++ srcs/expand/expand_quote.c
@@ -4,20 +4,13 @@ char 	find_first_quote(const char *str)
 {
 	int 	i;
 
-	i = 0;
-	while (str[i])
+	i = -1;
+	while (str[++i])
 	{
 		if (str[i] == '\'')
-		{
-			ft_putchar(str[i]);
 			return (str[i]);
-		}
 		else if (str[i] == '"')
-		{
-			ft_putchar(str[i]);
 			return (str[i]);
-		}
-		i++;
 	}
 	return (0);
 }
@@ -26,15 +19,11 @@ void	expand_antislash(t_string *string)
 {
 	int 	i;
 
-	i = 0;
-	while (string->s[i + 1])
+	i = -1;
+	while (string->s[++i])
 	{
-		if (string->s[i] == '\\' && string->s[i + 1] == '\\')
-		{
-			ft_printf("removed\n");
-			string_remove_char(string, i + 1);
-		}
-		i++;
+		if (string->s[i] == '\\')
+			string_remove_char(string, i);
 	}
 }
 
@@ -51,7 +40,7 @@ t_exp   *expand_remove_quote(t_exp *exp)
 	string.len = ft_strlen(string.s);
 	//remove '\'
 	if (exp->type != E_TOKEN_SQUOTE)
-	expand_antislash(&string);
+		expand_antislash(&string);
 	//remove the first of ' or "
 	if ((quote = find_first_quote(string.s)) != 0)
 		while ((c = ft_strchr(string.s, quote)))
