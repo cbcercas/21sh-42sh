b2869e54b256715f2ea9df4c0c50749f1d56a225
Unknown <guiforge@gmail.com>
2017-07-17 12:30:45 +0200

    [FEATURE]UI: now you can paste with texts from out of term

--- srcs/core/input.c
+++ srcs/core/input.c
@@ -6,7 +6,7 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 13:28:12 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/07/12 17:44:25 by gpouyat          ###   ########.fr       */
+/*   Updated: 2017/07/17 12:22:00 by guiforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,18 +73,26 @@ static void	tcaps_insert_char(char *c)
 
 static void	draw_char(t_input *input, char *c)
 {
-	// si je suis au milieu de la string j'insert le char sinon je le write
-	if (input->str->len > (((((input->ts.ws_col * input->offset_line) - input->offset_col)
-		+ (input->cpos.cp_col - 1 + input->offset_line )))))
-		tcaps_insert_char(c);
-	else
-		tputs(c, 1, &ft_putchar2);
-	input->cpos.cp_col += 1;
-	if (input->cpos.cp_col >= input->ts.ws_col)
-		tcaps_down(input);
-	// si je suis au milieu de la string je la redraw
-	if (input->str->len > ((((input->ts.ws_col * input->offset_line) + input->cpos.cp_col) - input->offset_col)))
-		redraw_line(input);
+	int 	len;
+
+	len = 0;
+	if (c)
+		len = ft_strlen(c);
+	redraw_line(input);
+	while (len --)
+		exec_arrow_right(NULL, input);
+			// si je suis au milieu de la string j'insert le char sinon je le write
+			/*if (input->str->len > (((((input->ts.ws_col * input->offset_line) - input->offset_col)
+				+ (input->cpos.cp_col - 1 + input->offset_line )))))
+				tcaps_insert_char(c);
+			else
+				tputs(c, 1, &ft_putchar2);
+			input->cpos.cp_col += 1;
+			if (input->cpos.cp_col >= input->ts.ws_col)
+				tcaps_down(input);
+			// si je suis au milieu de la string je la redraw
+			if (input->str->len > ((((input->ts.ws_col * input->offset_line) + input->cpos.cp_col) - input->offset_col)))
+				redraw_line(input);*/
 }
 
 char	*sh_get_line(t_sh_opt *opts)
