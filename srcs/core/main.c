90ecac309ae788f650cbd41ef3374db1a2f15934
Unknown <guiforge@gmail.com>
2017-07-18 17:29:28 +0200

    [FIX/REFACTOR]UI: Fix #25 + refactor main

--- srcs/core/main.c
+++ srcs/core/main.c
@@ -6,94 +6,107 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/15 19:36:55 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/07/14 16:12:43 by guiforge         ###   ########.fr       */
+/*   Updated: 2017/07/18 17:27:44 by guiforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <core/main.h>
 
+// permet de destroy, de print un messages, et d'exit
+void sh_over(char *mess, t_automaton *automat, t_array *array, void fn(void *elem))
+{
+	if (mess)
+		ft_putendl_fd(mess, 2);
+	if (automat)
+		automaton_destroy(&automat);
+	if (array)
+		array_destroy(&array, fn);
+	exit (EXIT_FAILURE);
+}
+
+// init tout les arrays qu'on a besoins
+void sh_arrays_init(t_automaton *automat, t_array *tokens, t_array *expands)
+{
+	if (lexer_init(tokens) == NULL)
+		sh_over("ERROR: Initialisation tokens", automat, tokens, NULL);
+	if (automaton_init(automat) == NULL)
+		sh_over("ERROR: Initialisation automaton", automat, NULL, NULL);
+	if (expand_init(expands) == NULL)
+		sh_over("ERROR: Initialisation expand", automat, expands, sh_exp_del);
+}
+
+//récupère l'input en fonction d'isatty
+BOOL sh_get_input(t_sh_data *data, char **input)
+{
+	if (!isatty(0))
+	{
+		if (!get_next_line(0, input))
+			return (false);
+		if (input)
+			ft_putendl(*input);
+	}
+	else
+		*input = sh_get_line(&(data->opts));
+	return (true);
+}
+
+//reset les arrays
+void sh_arrays_reset(t_automaton *automat, t_array *tokens, t_array *expands,\
+	 										char *input)
+{
+	sh_history_set_new(input);
+	array_reset(tokens, NULL);
+	array_reset(expands, sh_exp_del);
+	automaton_reset(automat);
+	input ? ft_strdel(&input) : 0;
+}
+
+//lex, pars, expand, et build ast, il retourn l'ast
+t_btree	*sh_process(t_automaton *automat, t_array *tokens, t_array *expands,\
+	 										char *input)
+{
+	t_btree		*ast;
+
+	ast = NULL;
+	if (lexer_lex(tokens, automat, input))
+		if (parser_parse(tokens))
+		{
+			lexer_print_tokens(tokens);
+			if (expand(tokens, expands))
+			{
+				if (!(ast = ast_create(expands)))
+					ft_printf("AST NULL\n");
+				else
+					btree_print(ast, (char * (*)(void*))&ast_aff);
+				return (ast);
+			}
+		}
+		return (NULL);
+}
+
+//j'ai eu beaucoup de mal à diminuer le nombre de variables 
 int main(int ac, char *const *av, char **environ)
 {
-
 	t_sh_data	data;
 	t_automaton	automaton;
 	t_array		tokens;
 	t_array		expand_array;
 	char		*input;
-	BOOL		stop;
-	t_btree	*ast;
-	//char cwd[1024];
 
-	/*sh_init_environ();
-	char *tmp = sh_getenv_value("LSCOLORS");
-	ft_printf("%s",tmp);*/
+	sh_arrays_init(&automaton, &tokens, &expand_array);
 	if (!sh_init(&data, ac, av, environ))
 		exit(1);
-	if (lexer_init(&tokens) == NULL)
-		exit (1);
-	if (automaton_init(&automaton) == NULL)
-		exit (1);
-	if (expand_init(&expand_array) == NULL)
-		exit (1);
-	stop = true;
-	while (stop == true)
+	while (42)
 	{
 		sh_print_prompt();
-		input = sh_get_line(&data.opts);
-		if (lexer_lex(&tokens, &automaton, input))
-			if (parser_parse(&tokens))
-			{
-				lexer_print_tokens(&tokens);
-				if (expand(&tokens, &expand_array))
-				{
-					if (!(ast = ast_create(&expand_array)))
-						ft_printf("AST NULL\n");
-					else
-						btree_print(ast, (char * (*)(void*))&ast_aff);
-					//expand_print(&expand_array);
-					/// if EXEC
-				}
-			}
-			sh_history_set_new(input);
-
-		// if ((command = ft_strsplit(input, ';')))
-		// 	if (sh_command(data, command))
-		// 		stop = true;
-
-		/******ECHO*****/
-		//char **test = ft_strsplit("echo -e toto\\\\n \\\\ntata", ' ');
-		//	sh_echo(NULL, test);
-		/***********/
-
-
-		/*****CD CHDIR******/
-		//if (input != NULL)
-		//	sh_chdir(NULL, ft_strsplit(input, ' '));
-		//getcwd(cwd, sizeof(cwd));
-		//ft_printf("%s", cwd);
-		/***********/
-
-
-		/******HELP*****/
-		//if (input)
-			//sh_builtin_help(&data, ft_strsplit(input, ' '));
-		/***********/
-
-		/******SETENV*****/
-		//if (input)
-			//sh_builtin_unsetenv(&data, ft_strsplit(input, ' '));
-		/***********/
-
+		if (!sh_get_input(&data, &input))
+			break;
+			/*sh_exec(*/sh_process(&automaton, &tokens, &expand_array, input);//); //TODO destroy ast
 		if (input && ft_strequ(input, "exit"))
-			stop = false;
-		input ? ft_strdel(&input) : 0;
-		array_reset(&tokens, NULL);
-		array_reset(&expand_array, sh_exp_del);
-		automaton_reset(&automaton);
+			break;
+		sh_arrays_reset(&automaton, &tokens, &expand_array, input);
 	}
-	sh_history_save();
-	//sh_expand_destroy(&expand_array);
-	sh_deinit(&data);
-	ft_putstr("\033[?1049l");
+	sh_arrays_reset(&automaton, &tokens, &expand_array, input);
+	sh_exit(&data, NULL);
 	return (0);
 }
