9f713ab7dc2e512aad7233ced8141d59fe842f40
Guillaume Pouyat <guiforge@gmail.com>
2017-07-19 18:03:48 +0200

    [FEATURE]Exec: better output signals

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -23,9 +23,9 @@ int		wait_sh(void)
 	if (WIFSIGNALED(status))
 	{
 		if (WTERMSIG(status) == SIGSEGV)
-			ft_printf("[[1]    %d segmentation fault\n", pid_child);//TODO add the last command with history
+			ft_printf("[1]    %d segmentation fault (core dumped)  %s\n", pid_child, sh_history_get_at(-1));
 		else if (WTERMSIG(status) == SIGBUS)
-			ft_printf("[1]    %d bus error\n", pid_child);//TODO add the last command with history
+			ft_printf("[1]    %d bus error  %s\n", pid_child, sh_history_get_at(-1));
 		return (-1);
 	}
 	return (status);
