bc62e9b833917a54f56646c14058cdb329b7fd1f
Guillaume Pouyat <guiforge@gmail.com>
2017-07-20 10:22:49 +0200

    [FIX]Exec: history fix set_new segfault

--- srcs/history/history_getter.c
+++ srcs/history/history_getter.c
@@ -6,7 +6,7 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/03 17:16:17 by gpouyat           #+#    #+#             */
-/*   Updated: 2017/07/20 10:14:18 by guiforge         ###   ########.fr       */
+/*   Updated: 2017/07/20 10:19:33 by guiforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,8 @@ t_hist	*sh_history_set_new(char const *cmd)
 			ft_strdel(&(h->buf));
 			h->cur = -1;
 		}
-		if (hists->used >= 1 && ft_strcmp(cmd, sh_history_get_at(-1)) && ((h = sh_history_new(ft_strdup(cmd))) != NULL))
+		if ((hists->used < 1 || ft_strcmp(cmd, sh_history_get_at(-1))) &&\
+		 ((h = sh_history_new(ft_strdup(cmd))) != NULL))
 		{
 			h->session = true;
 			array_push(hists, (void *)h);
