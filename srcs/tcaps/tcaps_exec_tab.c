4e52fef0b6ffcc9277447014159325b34cc3bad2
Jules Lasne (jlasne) <jlasne@student.42.fr>
2017-07-21 22:26:35 +0200

    [FEATURE] Autocomplete: Added functions to know if you're alone, first or after word in input

--- srcs/tcaps/tcaps_exec_tab.c
+++ srcs/tcaps/tcaps_exec_tab.c
@@ -14,18 +14,51 @@
 
 int is_input_empty(char *input)
 {
-	(void) input;
-	return (0);
+	if (input == NULL)
+		return (1);
+	else if (input[0] == '\0')
+		return (1);
+	else
+		return (0);
 }
 int is_input_at_first(char *input)
 {
-	(void)input;
-	return (0);
+	size_t len;
+	size_t i;
+
+	i = 0;
+	len = ft_strlen(input);
+
+	while (input[i] != '\0' && i <= len)
+	{
+		if (input[i] == ' ')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int is_input_after_first(char *input)
 {
-	(void)input;
+	size_t len;
+	size_t i;
+	size_t j;
+
+	len = ft_strlen(input);
+	i = 0;
+
+	while (input[i] != '\0')
+	{
+		if (input[i] == ' ')
+		{
+			j = i;
+			while (input[j] == ' ')
+				j--;
+			if (ft_isalnum(input[j]) == 1)
+				return (1);
+		}
+		i++;
+	}
 	return (0);
 }
 
@@ -39,7 +72,7 @@ BOOL	exec_tab(const t_key *key, t_input *input)
 		; //user pressed tab with a command started
 	if (is_input_after_first(input->str->s) == 1)
 		; //user pressed tab after writing the first command
-	ft_printf("\n|%s|\n", autoc_get_last(input->str->s));
+	ft_printf("\n|%s|\n", autoc_get_last_word(input->str->s));
 	//TODO: Savoir si c'est le premier mot, sans mot, ou deuxieme mot et + dans l'input
 	return (false);
 }
