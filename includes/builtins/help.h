86992a9944fad6923eb4cb23f4d2c7acb1d1bc12
Guillaume Pouyat <guiforge@gmail.com>
2017-08-04 14:54:54 +0200

    [FEATURE]Builtins: colors ;)

--- includes/builtins/help.h
+++ includes/builtins/help.h
@@ -6,6 +6,7 @@
 # define INC_21SH_HELP_H
 
 # include <core/data.h>
+# include <core/color.h>
 # include <core/main.h>//TODO: c'est pas terrible d'inclures un header qui inclue quasi tout on risque d'avoir des problem, lorsque qu'on include help.h
 
 int sh_builtin_help(t_sh_data *data, char **args);
