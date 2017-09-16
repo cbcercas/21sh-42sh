b0c5f66aeb83a723c91a8dac83d7c7e5d237eb5b
gpouyat <guiforge@gmail.com>
2017-09-16 21:20:46 +0200

    [FIX]Atocomplete: fix clean_term

--- srcs/tcaps/clean_term.c
+++ srcs/tcaps/clean_term.c
@@ -6,7 +6,7 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/15 16:33:54 by gpouyat           #+#    #+#             */
-/*   Updated: 2017/09/15 16:34:23 by gpouyat          ###   ########.fr       */
+/*   Updated: 2017/09/16 21:20:04 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void clean_term(void)
 
 	ts = get_term_size();
 	tputs(tgetstr("sc", NULL), 0, &ft_putchar2);
-		ft_printf("\n");
+	tputs(tgetstr("do", NULL), 0, &ft_putchar2);
 	nb_of_ligne = ts.ts_cols;
 	while(nb_of_ligne)
 	{
-		tputs(tgetstr("cr", NULL), 0, &ft_putchar2);
-		tputs(tgetstr("cd", NULL), 0, &ft_putchar2);
+			tputs(tgetstr("cr", NULL), 0, &ft_putchar2);
+			tputs(tgetstr("cd", NULL), 0, &ft_putchar2);
 		nb_of_ligne--;
 	}
 	tputs(tgetstr("rc", NULL), 0, &ft_putchar2);
