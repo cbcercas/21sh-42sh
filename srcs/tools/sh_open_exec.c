19bc96d7e29ce9bb4a69587fb13e6caa7301bbb9
gpouyat <guiforge@gmail.com>
2017-09-20 16:00:38 +0200

    [FIX]EXEC: redir

--- srcs/tools/sh_open_exec.c
+++ srcs/tools/sh_open_exec.c
@@ -6,7 +6,7 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/29 13:34:46 by gpouyat           #+#    #+#             */
-/*   Updated: 2017/09/08 18:58:29 by gpouyat          ###   ########.fr       */
+/*   Updated: 2017/09/20 15:59:44 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int sh_open_exec(t_btree *ast)
 	redir = (t_cmd *)ast->item;
 
 	if (redir->type == E_TOKEN_LESSGREAT && ft_strequ(redir->av[0], ">"))
-		fd = open(item->av[0], O_RDWR | O_CREAT, 0644);
+		fd = open(item->av[0], O_RDWR | O_CREAT | O_TRUNC, 0644);
 	else if (redir->type == E_TOKEN_LESSGREAT)
 		fd = open(item->av[0], O_RDWR, 0644);
 	else if ((redir->type == E_TOKEN_DLESS ) || (redir->type == E_TOKEN_DGREAT))
