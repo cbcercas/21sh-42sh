0e6590d4c355b7a053bf3fa44ce6b39d787ca801
Jules Lasne (jlasne) <jlasne@student.42.fr>
2017-10-11 11:02:36 +0200

    [DOC] Finished documenting the header files for builtins

--- includes/builtins/pwd.h
+++ includes/builtins/pwd.h
@@ -3,6 +3,13 @@
 
 # include <core/data.h>
 
+/**
+ * @file   builtin_pwd.c
+ *
+ * @brief  This file contains the functions for the pwd builtin
+ */
+
+
 int sh_builtin_pwd(t_sh_data *data, char **args);
 
 #endif
