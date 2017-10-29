d9dd7fef94cd52beea953cd146ee519c4106e1a7
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-10-29 23:40:36 +0100

    [REFACTOR]Ui: redraw_line

--- srcs/tcaps/tcaps_redraw_line.c
+++ srcs/tcaps/tcaps_redraw_line.c
@@ -1,58 +1,31 @@
 #include <core/tcaps.h>
-#include <core/input.h>
 
-//TODO Need a big rewrite
 void	redraw_line(t_input *input)
 {
-	char   *tmp;
+	char	*tmp;
 	int		len;
 	int		line;
 	int		col;
 
-	if (input->str->len == 0)
-		return ;
-	tmp = input->str->s + ((((input->cpos.cp_col + 1) - (input->offset_col + 1)) +
-		  (input->ts->ws_col * input->cpos.cp_line)));
-	len = (int)ft_strlen(tmp);
 	line = 0;
 	col = 0;
-	// clear rest of line if needed
-	tputs(tgetstr("ce", NULL), 0, ft_putchar2);
-	if ( len > (input->ts->ws_col - input->cpos.cp_col))
-	{
-		tputs(tgetstr("do", NULL), 0, ft_putchar2);
-		tputs(tgetstr("cr", NULL), 0, ft_putchar2);
-		tputs(tgetstr("cd", NULL), 0, ft_putchar2);
-		tputs(tgetstr("up", NULL), 0, ft_putchar2);
-		while (col < input->cpos.cp_col)
-		{
-			tputs(tgetstr("nd", NULL), 0, &ft_putchar2);
-			col += 1;
-		}
-	}
-	//affiche la ligne
-	tputs(tmp, (int) len, &ft_putchar2);
-	col = input->cpos.cp_col + len;
+	if (input->str->len == 0)
+		return ;
+	tmp = input->str->s + ((((input->cpos.cp_col + 1) - (input->offset_col + 1))
+							+ (input->ts->ws_col * input->cpos.cp_line)));
+	len = (int)ft_strlen(tmp);
+	tputs(tmp, len, &ft_putchar2);
 	if (len > (input->ts->ws_col - input->cpos.cp_col))
-	{
-		line =(len - (input->ts->ws_col - input->cpos.cp_col)) / input->ts->ws_col + ((len - (input->ts->ws_col - input->cpos.cp_col)) % input->ts->ws_col? 1 : 0);
-		col  = (len - (input->ts->ws_col - input->cpos.cp_col)) % input->ts->ws_col;
-	}
-	//TODO add tcaps_up(t_input *input, int nb)
-	while (line)
-	{
+		line = (len - (input->ts->ws_col - input->cpos.cp_col))
+						/ input->ts->ws_col + ((len - (input->ts->ws_col
+							- input->cpos.cp_col)) % input->ts->ws_col? 1 : 0);
+	while (line--)
 		tputs(tgetstr("up", NULL), 0, &ft_putchar2);
-		line -= 1;
-	}
 	tputs(tgetstr("cr", NULL), 0, ft_putchar2);
-	col = 0;
-	//TODO add tcaps_right(t_input *input, int nb)
-	while (col < input->cpos.cp_col)
-	{
+	while (col++ < input->cpos.cp_col)
 		tputs(tgetstr("nd", NULL), 0, &ft_putchar2);
-		col += 1;
-	}
 }
+
 
 void reset_line(t_input *input)
 {
