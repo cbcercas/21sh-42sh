aab79312dbdbc3c716e4798e1e3932da7ef9b982
gpouyat <guiforge@gmail.com>
2017-11-07 15:39:19 +0100

    [REFACTOR] UI: alt-arrows

--- srcs/tcaps/tcaps_exec_alt_arrows.c
+++ srcs/tcaps/tcaps_exec_alt_arrows.c
@@ -12,72 +12,76 @@
 
 #include <core/tcaps.h>
 
+BOOL	exec_alt_input(const t_key *key, t_input *input, unsigned short x)
+{
+	if (!is_alt_down_arrow(key->key) && !is_alt_down_arrow(key->key))
+			return (false);
+	if((!input->prev && is_alt_up_arrow(key->key)) ||
+			(!input->next && is_alt_down_arrow(key->key)))
+		return (false);
+	input->cpos = (is_alt_up_arrow(key->key) ? input_get_first_pos(input) :
+				   input_get_last_pos(input));
+	move_cursor_to(&input->cpos
+			, &(t_cpos){input->cpos.cp_col, input->cpos.cp_line}, get_ts());
+	if (is_alt_up_arrow(key->key))
+	{
+		exec_arrow_left(NULL, input);
+		input = input->prev;
+	}
+	else
+	{
+		input = input->next;
+		exec_arrow_right(NULL, input);
+	}
+	if (input_get_last_pos(input).cp_col < x)
+		input->cpos = input_get_last_pos(input);
+	else
+		input->cpos.cp_col = x;
+	move_cursor_to(&input->cpos
+			, &(t_cpos){input->cpos.cp_col, input->cpos.cp_line}, get_ts());
+	return (false);
+}
+
 BOOL	exec_alt_up(const t_key *key, t_input *input)
 {
-	struct winsize	*ts;
-	size_t    x;
+	unsigned short    x;
 
-	(void)key;
 	log_dbg1("exec alt arrow up.");
-	ts = get_ts();
+	if (get_select()->is)
+		return (false);
 	x = input->cpos.cp_col;
 	if (!(input->cpos.cp_line <= 0 || (input->cpos.cp_line == 1 &&
 													x < input->offset_col)))
 	{
 		move_cursor_up(&input->cpos);
+		move_cursor_to(&input->cpos, &(t_cpos){input->cpos.cp_col, input->cpos.cp_line}, get_ts());
 		return (false);
 	}
-	if (!input->prev)
-		return (false);
-	input = input->prev;
-	tputs(tgetstr("up", NULL), 0, &ft_putchar2);
-	input->cpos = input_get_last_pos(input);
-	move_cursor_to(&input->cpos, &(t_cpos){input->next->cpos.cp_col,
-										   input->cpos.cp_line}, get_ts());
-	if (pos_in_str(input->next) < input->str->len)
-		while (input->cpos.cp_col != input->next->cpos.cp_col)
-			move_cursor_left(&input->cpos, ts);
-	get_windows(0)->cur = input;
-	return (false);
-}
-
-static	BOOL	alt_down_simple(t_input *input, struct winsize	*ts)
-{
-	size_t			save_col;
-
-	save_col = input->cpos.cp_col;
-	if (((input->str->len + input->prompt_len) / ts->ws_col) - 1 != (input->cpos.cp_line))
-	{
-		move_cursor_down(&input->cpos);
-		return (false);
-	}
-	move_cursor_right(&(input->cpos), ts);
-	while (input->cpos.cp_col != save_col && pos_in_str(input) < input->str->len)
-		move_cursor_right(&(input->cpos), ts);
-	return (false);
+	return (exec_alt_input(key, input, x));
 }
 
 BOOL	exec_alt_down(const t_key *key, t_input *input)
 {
-	struct winsize	*ts;
+	unsigned short    x;
 
-	(void) key;
 	log_dbg1("exec alt arrow down.");
-	ts = get_ts();
-	if ((input->str->len + input->prompt_len) / ts->ws_col != input->cpos.cp_line)
-		return (alt_down_simple(input, ts));
-	if (!input->next)
+	if (get_select()->is)
 		return (false);
-	input = input->next;
-	tputs(tgetstr("do", NULL), 0, &ft_putchar2);
-	input->cpos = input_get_last_pos(input);
-	move_cursor_to(&input->cpos, &(t_cpos){input->prev->cpos.cp_col,
-										   input->cpos.cp_line}, get_ts());
-	if (pos_in_str(input->prev) < input->str->len)
-		while (input->cpos.cp_col != input->prev->cpos.cp_col)
-			move_cursor_right(&input->cpos, ts);
-	get_windows(0)->cur = input;
-	return (false);
+	x = input->cpos.cp_col;
+	if (input->cpos.cp_line != input_get_last_pos(input).cp_line)
+	{
+		if (input->cpos.cp_line == input_get_last_pos(input).cp_line - 1)
+		{
+			move_cursor_right(&(input->cpos), get_ts());
+			while (x != input->cpos.cp_col && pos_in_str(input) < input->str->len)
+				move_cursor_right(&(input->cpos), get_ts());
+			return (false);
+		}
+		move_cursor_down(&input->cpos);
+		move_cursor_to(&input->cpos, &(t_cpos){input->cpos.cp_col, input->cpos.cp_line}, get_ts());
+		return (false);
+	}
+	return (exec_alt_input(key, input, x));
 }
 
 BOOL	exec_alt_left(const t_key *key, t_input *input)
