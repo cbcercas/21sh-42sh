483d1f76808b24c0de220720f3216cd41f60c95b
gpouyat <guiforge@gmail.com>
2017-12-05 10:08:25 +0100

    [FIX] master: fix valgrind tests

--- tests/tests_bats/test_helper.bash
+++ tests/tests_bats/test_helper.bash
@@ -50,22 +50,21 @@ fi
 check_leaks_function()
 {
 	if [[ $TESTS_CHECK_LEAKS -ne 0 ]]; then
-		leaks_direct=$(grep "definitely lost:" check_leaks_tmp.log | grep "0 bytes in 0 blocks" | wc -l | sed -e 's/1/Yes/g' -e 's/0/No/g')
-		leaks_indirect=$(grep "indirectly lost:" check_leaks_tmp.log | grep "0 bytes in 0 blocks" | wc -l | sed -e 's/1/Yes/g' -e 's/0/No/g')
-		leaks_reach=$(grep "still reachable:" check_leaks_tmp.log | grep "0 bytes in 0 blocks" | wc -l | sed -e 's/1/Yes/g' -e 's/0/No/g')
+		leaks_direct=$(grep "definitely lost:" check_leaks_tmp.log | grep "0 bytes in 0 blocks" | wc -l | sed -e 's/1/Yes/g' -e 's/0/No/g' | tr -d ' ')
+		leaks_indirect=$(grep "indirectly lost:" check_leaks_tmp.log | grep "0 bytes in 0 blocks" | wc -l | sed -e 's/1/Yes/g' -e 's/0/No/g' | tr -d ' ')
+		leaks_reach=$(grep "still reachable:" check_leaks_tmp.log | grep "0 bytes in 0 blocks" | wc -l | sed -e 's/1/Yes/g' -e 's/0/No/g' | tr -d ' ')
 		display_error_leaks $1
-		echo "toto = |$leaks_direct|"
-		if [ "$leaks_direct" = "       No" ] || [ $leaks_indirect = "       No" ]; then
+		if [ "$leaks_direct" = "No" ] || [ $leaks_indirect = "No" ]; then
 			mkdir -p "${BATS_TEST_DIRNAME}/../valgrind_log/$1"
 			cp check_leaks_tmp.log ${BATS_TEST_DIRNAME}/../valgrind_log/$1/TEST-$BATS_TEST_NUMBER-AST.log
-		elif [ "$leaks_reach" = "       No" ] && [ $TESTS_CHECK_LEAKS -eq 2 ]; then
+		elif [ "$leaks_reach" = "No" ] && [ $TESTS_CHECK_LEAKS -eq 2 ]; then
 			echo "[-- STILL REACHABLE!! --]"
 			mkdir -p "${BATS_TEST_DIRNAME}/../valgrind_log/$1"
 			cp check_leaks_tmp.log ${BATS_TEST_DIRNAME}/../valgrind_log/$1/TEST-$BATS_TEST_NUMBER-AST.log
-			[ "$leaks_reach" = "       No" ]
+			[ "$leaks_reach" = "No" ]
 		fi
-		[ "$leaks_direct" = "       Yes" ]
-		[ "$leaks_indirect" = "       Yes" ]
+		[ "$leaks_direct" = "Yes" ]
+		[ "$leaks_indirect" = "Yes" ]
 	fi
 }
 
