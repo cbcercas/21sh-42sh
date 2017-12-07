6af0b00300a287f74e70a333e19c6d549daa796b
mleroy <mleroy@student.42.fr>
2017-12-07 17:23:55 +0100

    [FEATURE] expand: wip BQUOTE

--- srcs/expand/expand_bquote.c
+++ srcs/expand/expand_bquote.c
@@ -6,7 +6,7 @@
 /*   By: mleroy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 14:21:42 by mleroy            #+#    #+#             */
-/*   Updated: 2017/12/07 15:36:42 by mleroy           ###   ########.fr       */
+/*   Updated: 2017/12/07 16:46:53 by mleroy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 void		*expand_bquote(t_string *str)
 {
+	int		tube[2];
+	char	buffer[4096];
+
+	sh_pipe(tube);
+	dup2(STDOUT_FILENO, tube[0]);
+	//delete//////////////////////////////////////////
 	ft_putnstr(str->s, str->len);
 	ft_putchar(10);
+	//////////////////////////////////////////////////
+	while (get_next_line(tube[1], &buffer) == 1)
+	{
+		ft_printf("buffer=>%s\n", buffer);
+	}
 }
